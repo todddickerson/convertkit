4587195277d830d0e58d671e9ca4ac5074c3fd54
Sergey Chernyakov <chernyakov.sergey@gmail.com>
2017-09-13 19:44:57 +0200

    Added tags

---
+++ lib/convertkit/tag.rb
@@ -0,0 +1,45 @@
+module ConvertKit
+  class Tag
+
+    attr_reader :id, :name, :created_at
+    attr_writer :client
+
+    def self.find(id, client)
+      raw  = client.get_request("/tags/#{id}")
+      course = ConvertKit::Tag.new(id, client)
+      course.load(raw, client)
+
+      course
+    end
+
+    def load(data, client)
+      @client = client
+
+      @id                = data["id"]
+      @name              = data["name"]
+      @created_at        = data["created_at"]
+    end
+
+    def initialize(id, client)
+      @id     = id
+      @client = client
+    end
+
+  end
+
+  class Client
+    def tags()
+      raw   = get_request("/tags")
+      tags = []
+
+      raw.each do |raw_tag|
+        tag = ConvertKit::Tag.new(raw_tag["id"], self)
+        tag.load(raw_tag, self)
+
+        tags << tag
+      end
+
+      tags
+    end
+  end
+end
